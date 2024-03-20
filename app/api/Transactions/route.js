75b993e6afbd651f7bee83108230faed97ef7fc9
fumimelvindev <fumimelvindev@gmail.com>
2024-03-20 13:45:46 +0200

    get transactions api, and dateFormatter

--- app/api/Transactions/route.js
+++ app/api/Transactions/route.js
@@ -2,7 +2,6 @@ import Transaction from "@/lib/mongoDB/models/transaction";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  console.log("first request");
   try {
     const body = await req.json();
 
@@ -10,6 +9,17 @@ export async function POST(req) {
     await Transaction.create(transactionData);
 
     return NextResponse.json({ message: "success" }, { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "error", error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const transactions = await Transaction.find();
+
+    return NextResponse.json({ transactions }, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "error", error }, { status: 500 });
