ed1c20a6f2b21a57b91640b9f5b28a8744188413
fumimelvindev <fumimelvindev@gmail.com>
2024-03-20 13:46:11 +0200

    transaction card

--- app/components/TransactionCard.jsx
+++ app/components/TransactionCard.jsx
@@ -1,7 +1,24 @@
+import { currencyFormatter, dateFormatter } from "@/lib/utils";
 import React from "react";
 
-function TransactionCard() {
-  return <div>TransactionCard</div>;
+async function TransactionCard({ transaction }) {
+  return (
+    <section className="border rounded-full px-4 py-1 bg-slate-500">
+      <div className="flex items-center justify-between">
+        <div className="flex space-x-2 items-center">
+          <div
+            className="h-6 w-6 rounded-full"
+            style={{ backgroundColor: transaction.color }}
+          />
+          <h1 className="capitalize font-medium">{transaction.description}</h1>
+          <small className="">{dateFormatter(transaction.createdAt)}</small>
+        </div>
+        <div className="flex space-x-2">
+          <p className="text-sm">{currencyFormatter(transaction.amount)}</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default TransactionCard;
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,34 @@
 import AddTransaction from "./components/AddTransaction";
 import TransactionCard from "./components/TransactionCard";
 
-export default function Home() {
+const getTransactions = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Transactions", {
+      cache: "no-store",
+    });
+
+    return res.json();
+  } catch (error) {
+    console.log("Failed to fetch transactions", error);
+  }
+};
+
+export default async function Home() {
+  const { transactions } = await getTransactions();
+
   return (
     <main className="min-h-screen justify-between px-10 sm:px-52 mx-auto">
       <div className="grid md:grid-cols-2 gap-6">
         <div className="">
           <AddTransaction />
-          <TransactionCard />
+          <div className="space-y-3 py-4">
+            {transactions.map((transaction) => (
+              <TransactionCard
+                key={transaction._id}
+                transaction={transaction}
+              />
+            ))}
+          </div>
         </div>
         <div className="">stats</div>
       </div>
